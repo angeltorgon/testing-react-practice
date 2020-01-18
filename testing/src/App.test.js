da0f83e4af99d424e1dc13916a88deca415b94e4
angel-torres <alas4224@gmail.com>
2020-01-17 22:21:43 -0600

    added a snapshot test

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  test('should match snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
